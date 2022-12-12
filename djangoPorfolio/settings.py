1413803d27fde0ede716c727cb0fe42df7403114
Raisa320 <67403486+Raisa320@users.noreply.github.com>
2022-12-11 19:37:03 -0500

    static

--- djangoPorfolio/settings.py
+++ djangoPorfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n9lb0e=&2*ha4v5vlq20v_h$@@&_1$9nd%qca40l9#fez1q)6!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -124,7 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-#STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
 STATIC_URL = 'static/'
 
 # Default primary key field type
